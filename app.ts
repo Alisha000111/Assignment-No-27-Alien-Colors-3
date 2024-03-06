fda7eebe461e63a9cde066823af925d18dac01ba
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-06 13:50:37 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,47 @@
+let alien_color: string = "green";
+// If the alien is green, Print a message that the player earned 5 points.
+if(alien_color == "green"){
+    console.log("the player earned 5 points.");
+}
+// If the alien is yellow, Print a message that the player earned 10 points.
+else if(alien_color == "yellow"){
+    console.log("the player just earned 10 points.");
+}
+// If the alien is red, Print a message that the player earned 15 points.
+else if(alien_color == "red"){
+    console.log("the player just earned 10 points.");
+}
+else{
+    console.log("Please select right color")
+}
+// Write three versions of yhis program, making sure
+// Each message is printed for the appropriate color alien.
+// version 2
+alien_color = "red";
+if(alien_color == "green"){
+    console.log("the player earned 5 points.");
+}
+else if(alien_color == "yellow"){
+    console.log("the player just earned 10 points.");
+}
+else if(alien_color == "red"){
+    console.log("the player just earned 15 points.");
+}
+else{
+    console.log("Please select right color")
+}
+// version 3
+alien_color = "yellow";
+if(alien_color == "green"){
+    console.log("the player earned 5 points.");
+}
+else if(alien_color == "yellow"){
+    console.log("the player just earned 10 points.");
+}
+else if(alien_color == "red"){
+    console.log("the player just earned 15 points.");
+}
+else{
+    console.log("Please select right color")
+}
+
